ce6a8bd1686a1f1ccadedef10f9782cbe4e5e7ba
abombin <abombin93@gmail.com>
2022-06-14 21:11:47 -0400

    organized code in functions, added searches for samples, location, and dates

--- programs/darft1.py
+++ programs/darft1.py
@@ -21,12 +21,13 @@ password=getInfo(gisaidPassword)
 driver=webdriver.Chrome(drPath)
 driver.get(gisaidPath)
 
-driver.find_element_by_id('elogin').send_keys(login)
-driver.find_element_by_id('epassword').send_keys(password)
+def logIn(login, password):
+    driver.find_element_by_id('elogin').send_keys(login)
+    driver.find_element_by_id('epassword').send_keys(password)
+    driver.find_elements_by_class_name('form_button_submit')[0].click()
+    time.sleep(5)
+    print('Logged in')
 
-driver.find_elements_by_class_name('form_button_submit')[0].click()
-
-time.sleep(10) # sleep to deal with pop up manually
 
 def clearPopUp():
     try:
@@ -47,24 +48,83 @@ def clearWindow():
     except:
         pass
 
-clearWindow()
-
-time.sleep(3)
-
-driver.find_element_by_xpath('/html/body/form/div[5]/div/div[2]/div/div[1]/div/div/div[3]')\
-    .click() # clcik on the search option
-
-time.sleep(5)
-
-
-def filterNme():
+def epiSearch():
     try:
-        driver.find_element_by_id('ce_rdf3cu_rv_entry').send_keys('hCoV-19/USA/GA-EHC')
+        driver.find_element_by_xpath('/html/body/form/div[5]/div/div[1]/div/div/div/div/div[2]/div/ul/li[3]/a')\
+            .click()
     except:
         pass
+    time.sleep(3)
+    try:
+        driver.find_element_by_xpath('/html/body/form/div[5]/div/div[2]/div/div[1]/div/div/div[3]')\
+            .click() # clcik on the search option
+    except:
+        pass
+    time.sleep(5)
+    print('Went EpiSearch')
 
-filterNme()
 
-time.sleep(15)
 
-driver.find_element_by_xpath('/html/body/form/div[5]/div/div[2]/div/div[2]/div[2]/div[1]/div[2]/table/thead/tr/th[1]/div/span/input').click()
+sampName='hCoV-19/USA/GA-EHC'
+
+def filterName(sampName):
+    try:
+        driver.find_element_by_id('ce_rdf3cu_rv_entry').send_keys(sampName)
+    except:
+        pass
+    time.sleep(10)
+    print('Filtered by Name')
+
+location='North America / USA / Georgia'
+
+def filterLocation(location):
+    try:
+        driver.find_element_by_id('ce_rdf3cu_rx_entry').send_keys(location)
+    except:
+        pass
+    time.sleep(10)
+
+#day= '2022-06-14'
+def filterDate(day):
+    try:
+       driver.find_element_by_id('ce_rdf3cu_s1_input').send_keys(day) 
+    except:
+        pass
+    time.sleep(5)
+    try:
+        driver.find_element_by_id('ce_rdf3cu_s2_input').send_keys(day)
+    except:
+        pass
+    time.sleep(5)
+
+
+def download():
+    try:
+        driver.find_element_by_xpath('/html/body/form/div[5]/div/div[2]/div/div[2]/div[2]/div[1]/div[2]/table/thead/tr/th[1]/div/span/input')\
+            .click()
+    except:
+        pass
+    time.sleep(15)
+    driver.find_element_by_xpath('/html/body/form/div[5]/div/div[2]/div/div[2]/div[2]/div[1]/div[2]/table/thead/tr/th[1]/div/span/input')\
+        .click()
+    time.sleep(10)
+
+# run functions
+#logIn(login, password)
+#epiSearch()
+#filterName(sampName)
+#filterLocation(location)
+#filterDate(day)
+#download()
+days= ['2022-06-14', '2022-06-15']
+
+def runDays():
+    logIn(login, password)
+    epiSearch()
+    filterName(sampName)
+    for i in days:
+        day=i
+        filterDate(day)
+        download()
+
+
